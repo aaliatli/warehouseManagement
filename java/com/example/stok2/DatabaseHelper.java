566c7fab2e2e314d4462d3c0ba7b441929553d47
Ali Atlı <120938151+aaliatli@users.noreply.github.com>
2025-01-15 13:09:57 +0300

    Update DatabaseHelper.java

--- java/com/example/stok2/DatabaseHelper.java
+++ java/com/example/stok2/DatabaseHelper.java
@@ -1,5 +1,4 @@
-package com.example.stok2;
-
+package com.example.myproject;
 
 import android.content.ContentValues;
 import android.content.Context;
@@ -14,13 +13,13 @@ import java.util.List;
 public class DatabaseHelper extends SQLiteOpenHelper {
 
     private static final String DATABASE_NAME = "products.db";
-    private static final int DATABASE_VERSION = 1;
-
+    private static final int DATABASE_VERSION = 2;
     private static final String TABLE_PRODUCTS = "products";
     private static final String COLUMN_ID = "id";
     private static final String COLUMN_NAME = "name";
     private static final String COLUMN_DESCRIPTION = "description";
     private static final String COLUMN_STOCK = "stock";
+    private static final String COLUMN_BARCODE = "barcode";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -32,54 +31,50 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
                 + COLUMN_NAME + " TEXT, "
                 + COLUMN_DESCRIPTION + " TEXT, "
+                + COLUMN_BARCODE + " TEXT, "
                 + COLUMN_STOCK + " INTEGER)";
         db.execSQL(CREATE_PRODUCTS_TABLE);
     }
 
-
-    public void updateProduct(int id, String name, String description, int stock) {
-        SQLiteDatabase db = this.getWritableDatabase(); // Veritabanı bağlantısını açıyoruz
-
-        ContentValues values = new ContentValues();
-        values.put(COLUMN_NAME, name);  // Ürün adı
-        values.put(COLUMN_DESCRIPTION, description);  // Ürün açıklaması
-        values.put(COLUMN_STOCK, stock);  // Ürün stoğu (stock_amount değil, COLUMN_STOCK olmalı)
-
-        // Veritabanındaki ürünü güncelliyoruz
-        int rowsUpdated = db.update(TABLE_PRODUCTS, values, "id = ?", new String[]{String.valueOf(id)});
-
-        if (rowsUpdated > 0) {
-            Log.d("Database", "Ürün başarıyla güncellendi.");
-        } else {
-            Log.d("Database", "Ürün güncellenemedi.");
-        }
-
-        db.close();
-    }
-
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("DROP TABLE IF EXISTS " + TABLE_PRODUCTS);
-        onCreate(db);
+        if (oldVersion < 2) {
+            db.execSQL("ALTER TABLE " + TABLE_PRODUCTS + " ADD COLUMN " + COLUMN_BARCODE + " TEXT");
+        }
     }
 
-    public void addProduct(String name, String description, int stock) {
+    public void addProduct(String name, String description, int stock, String barcode) {
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues values = new ContentValues();
         values.put(COLUMN_NAME, name);
         values.put(COLUMN_DESCRIPTION, description);
         values.put(COLUMN_STOCK, stock);
+        values.put(COLUMN_BARCODE, barcode);
 
         db.insert(TABLE_PRODUCTS, null, values);
         db.close();
+    }
+
+    public boolean updateProduct(int id, String name, String description, int stock, String barcode) {
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues values = new ContentValues();
+        values.put(COLUMN_NAME, name);
+        values.put(COLUMN_DESCRIPTION, description);
+        values.put(COLUMN_STOCK, stock);
+        values.put(COLUMN_BARCODE, barcode);
+
+        int rowsUpdated = db.update(TABLE_PRODUCTS, values, COLUMN_ID + " = ?", new String[]{String.valueOf(id)});
+
+        db.close();
+        return rowsUpdated > 0;
     }
 
     public List<Product> getAllProducts() {
         List<Product> productList = new ArrayList<>();
         SQLiteDatabase db = this.getReadableDatabase();
 
-        Cursor cursor = db.query(TABLE_PRODUCTS, null, null, null, null, null, null);
+        Cursor cursor = db.query(TABLE_PRODUCTS, null, null, null, null, null, COLUMN_ID + " DESC");
 
         if (cursor.moveToFirst()) {
             do {
@@ -87,8 +82,8 @@ public class DatabaseHelper extends SQLiteOpenHelper {
                 String name = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME));
                 String description = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DESCRIPTION));
                 int stock = cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_STOCK));
-
-                productList.add(new Product(id, name, description, stock));
+                String barcode = cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_BARCODE));
+                productList.add(new Product(id, name, description, stock, barcode));
             } while (cursor.moveToNext());
         }
 
@@ -97,9 +92,38 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return productList;
     }
 
+    public Product getProductById(String barcode) {
+        SQLiteDatabase db = this.getReadableDatabase();
+        Product product = null;
+
+        Cursor cursor = db.query(
+                TABLE_PRODUCTS,
+                new String[]{COLUMN_ID, COLUMN_NAME, COLUMN_DESCRIPTION, COLUMN_STOCK, COLUMN_BARCODE},
+                COLUMN_BARCODE + " = ?",
+                new String[]{barcode},
+                null, null, null
+        );
+
+        if (cursor != null && cursor.moveToFirst()) {
+            product = new Product(
+                    cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_ID)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_NAME)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_DESCRIPTION)),
+                    cursor.getInt(cursor.getColumnIndexOrThrow(COLUMN_STOCK)),
+                    cursor.getString(cursor.getColumnIndexOrThrow(COLUMN_BARCODE))
+            );
+            cursor.close();
+        }
+
+        db.close();
+        return product;
+    }
+
     public void deleteProduct(int id) {
         SQLiteDatabase db = this.getWritableDatabase();
-        db.delete(TABLE_PRODUCTS, COLUMN_ID + "=?", new String[]{String.valueOf(id)});
+        db.delete(TABLE_PRODUCTS, COLUMN_ID + " = ?", new String[]{String.valueOf(id)});
         db.close();
     }
+
+
 }
